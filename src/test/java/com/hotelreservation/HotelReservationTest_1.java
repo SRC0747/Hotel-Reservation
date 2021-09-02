635709ed4a73c9ad4ad01fc20221b870e041370e
SRC0747 <RjSrc@0747>
2021-09-02 20:13:49 +0530

    added: Calculating the Cheapest Hotel as per 2 days Booking.

--- src/test/java/com/hotelreservation/HotelReservationTest_1.java
+++ src/test/java/com/hotelreservation/HotelReservationTest_1.java
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HotelReservationTest_1{
+    //Checking the Hotel Details.
     HotelReservation obj = new HotelReservation("LakeWood","Regular");
+    //Checking the Cheapest Hotel.
+    HotelReservation cheap_hotel = new HotelReservation();
 
     @Test
     public void toCheck_HotelDetails() {
         obj.hotelDetails();
+    }
+
+    @Test
+    public void toCheck_CheapHotel() {
+        cheap_hotel.cheapestHotel();
     }
 }
